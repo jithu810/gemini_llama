77a66bdcdb58245f812ea838bf58133943b5c3c3
jithu810 <sreejithg34@gmail.com>
2025-07-11 16:47:57 +0530

    latest pushs

---
+++ utils/constants.py
@@ -0,0 +1,14 @@
+# config/constants.py
+
+USE_SERVER = False  
+
+API_URL_MINICPM = "" 
+API_URL_LLAMA = ""
+HEADER_TOKEN = ""  
+LOCAL_MODEL_NAME = "Weights\models--openbmb--MiniCPM-V-2_6-int4"
+QUANTIZED = True 
+TEMPERATURE = 0.3
+MAX_NEW_TOKENS = 128
+ENVIRONMENT = "test"
+PORT=50051
+API_URL_MINICPM_BASE="https://sreejith8100-llm-fastapi.hf.space/predict"
